feb7d563397eb7326e714d45e129a48143de7b06
jonimahendra <82696768+jonimahendra@users.noreply.github.com>
2021-04-23 22:23:58 +0700

    Add files via upload

---
+++ Pert4_perkalian.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Pertemuan 4</h1>
+
+<?php
+echo "Dari latihan diatas apa perbedaan perintah break dan continue, yaitu Perintah continue digunakan untuk men-interupsi perulangan dalam PHP, namun jika perintah break digunakan untuk menghentikan perulangan, maka perintah continue hanya akan menghentikan perulangan untuk 1 iterasi saja, lalu proses perulangan akan dilanjutkan.";
+?>
+
+</body>
+</html>
